09a7e0ef85828d4ac913137e89cd2f332d7dcd09
Emanuela Busuioc <emanuela.busuioc@student.upt.ro>
2018-11-11 22:09:24 +0200

    First assignment

--- Hello/src/main/java/FirstProgram.java
+++ Hello/src/main/java/FirstProgram.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class FirstProgram {
 
     public static void main (String[] args){
-        System.out.printf("Talk is cheap. Show me the code.");
+        int number;
+        Scanner sc = new Scanner(System.in);
+        int[] NumbArray=new int[6];
+        System.out.printf("Inserati 6 numere naturale:\n");
+        for(int i=0;i<6;i++){
+          number = sc.nextInt();
+          NumbArray[i]=number;
+        }
+
+        for( Integer n: NumbArray){
+            System.out.printf("%d ",n);
+        }
     }
 }
